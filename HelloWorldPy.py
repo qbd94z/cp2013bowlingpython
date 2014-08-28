3517c70bda2e13e5eec111dfa825b1a370429d8b
qbd94z <benjamin_daly@live.com.au>
2014-08-28 10:22:37 +1000

    Python App
    Hello World added to app

---
+++ HelloWorldPy.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello World")
+    # Note Program ends as soon as code is complete.
+    # So user input is required to prevent the program from ending to soon. 
+    str(input("Press Enter Key to close Program."))
+main()
