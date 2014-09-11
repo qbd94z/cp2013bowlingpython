018a2fd7c6e20fa5e93093f7f50b9def8e534676
qbd94z <benjamin_daly@live.com.au>
2014-09-11 12:30:51 +1000

    Removal
    Removed Redundant Files

--- HelloWorldPy.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello World")
-    # Note Program ends as soon as code is complete.
-    # So user input is required to prevent the program from ending to soon. 
-    str(input("Press Enter Key to close Program."))
-main()
